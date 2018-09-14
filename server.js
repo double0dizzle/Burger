998287d3a6176ed75dfa99755c468f23bac2eab9
Kristin Herrmann <dickmann.michael@gmail.com>
2018-09-14 02:21:39 -0600

    new files

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+var mysql = require("mysql");
 var express = require("express");
 var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
 
 // Create an instance of the express app.
 var app = express();
